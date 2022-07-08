ec7748d939b75394c633afbc0c0f8b76ff7c830a
TDowling <tpdowling@proton.me>
2022-07-07 21:45:47 -0400

    barebones structure

---
+++ mixedmessages.js
@@ -0,0 +1,27 @@
+//                   Portfolio Project: Mixed Messages
+//                          Variable Declarations
+//Empty Array To Store End Message Result
+let personalMessage = [];
+
+//Dictionary Of Possible Terms
+const messageDict = {
+    key1: [],
+    key2: [],
+    key3: [],
+    key4: []
+}
+//                              Functions
+//Generates Random Number from 0 to (num-1)
+function generateRandomNumber(num){
+    return Math.floor(Math.random() * num);
+}
+
+function mainMessage() {
+
+}
+
+function secondaryMessage() {
+
+}
+
+//                              Main Code
